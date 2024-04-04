e0bb83a6acc41abf3271cb9bc799b6e5bdbe0b04
Arshan Rafid <arshan.rafid@stonybrook.edu>
2024-04-04 17:08:48 -0400

    thirdtimeidk

--- part3_question1.c
+++ part3_question1.c
@@ -1,1 +1,20 @@
+// Arshan Rafid (SBU ID: 115871065)
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define size 10
+
+int main(){
+    char str[size + 1];
+    printf("Input: ");
+    scanf("%s", &str);
+    printf("Output: \n");
+    for (int i = 0; i < strlen(str); i++){
+        for (int j = i; j < strlen(str); j++){
+            printf("%c", str[j]);
+        }
+        printf("\n");
+    }
+}
