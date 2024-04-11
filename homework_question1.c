cc56522056d31f50331bd05995df868d78120903
Arshan <arshan.rafid@stonybrook.edu>
2024-04-11 12:51:17 -0400

    submission4 probably

--- homework_question1.c
+++ homework_question1.c
@@ -1,1 +1,77 @@
+// Arshan Rafid (SBU ID: 115871065)
 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+int my_vowel(char letter){ // change this to vowels not uppercase
+    if (letter == 'A' || letter == 'a' || letter == 'E' || letter == 'e' || letter == 'I' || letter == 'i' || letter == 'O' || letter == 'o' || letter == 'U' || letter == 'u'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+//concatenate function
+
+char *my_strcat(char str[], char str2[]){
+    int j = 0;
+
+    while ((str2[j]) != '\0'){ // need length of second string
+        j++;
+    }
+
+    int k= 0;
+    while ((str[k]) != '\0'){ // need length of second string
+        k++;
+    }
+
+    for (int i = 0; i < j; i++){
+        str[k+i] = str2[i];
+    }
+    return str;
+}
+
+char *ToPigLatin(char str[]){
+    int i = 0;
+    char way[4] = {'w','a','y','\0'};
+    char ay[3] = {'a', 'y', '\0'};
+    char empty[MAX];
+    char finalstring[MAX];
+
+    if (str[i] == my_vowel(str[i])){
+        my_strcat(str, way);
+        my_strcat(finalstring, str);
+    }else {
+        char cons[MAX];
+        while (my_vowel(str[i]) == 0 && str[i] != 'Y' && str[i] != 'y'){
+            //my_strcat(cons, str[i]);
+            cons[i] = str[i];
+            i++;
+        }
+        while (str[i] != '\0'){ //after a vowel is found
+            //my_strcat(finalstring, str[i]);
+            finalstring[i] = str[i];
+            i++;
+        }
+        my_strcat(finalstring, cons);
+        my_strcat(finalstring, ay);
+    }
+
+    finalstring[i] = '\0';
+    return finalstring;
+}
+
+int main(){
+    char str1[MAX];
+    char str2[MAX];
+    char str3[MAX];
+    char str4[MAX];
+    char str5[MAX];
+
+    printf("Enter 5 words");
+    scanf("%s %s %s %s %s", &str1, &str2, &str3, &str4, &str5);
+    printf("%s\n%s\n%s\n%s\n%s\n", ToPigLatin(str1), ToPigLatin(str2), ToPigLatin(str3), ToPigLatin(str4), ToPigLatin(str5));
+
+}
--- part3_question2.c
+++ part3_question2.c
@@ -1,1 +1,54 @@
+// Arshan Rafid (SBU ID: 115871065)
 
+#include <stdio.h>
+#include <stdlib.h>
+
+char *same_char_remove(char str[], char str2[]){
+    int l = 0;
+    while ((str2[l]) != '\0'){ // need length of second string
+        l++;}
+
+    int k= 0;
+    while ((str[k]) != '\0'){ // need length of first string
+        k++;}
+
+    int i = 0; int j = 0;
+
+    if (k >= l){ //if the first string is bigger than the second
+        for (i = 0; i < k; i++){ //iterate through first string length
+        int instr = 0;
+        for (j = 0; j < l; j++){ // iterate through second string length
+            if (str2[j] == str[i]){
+                instr = 1;
+                break;
+            }
+        } 
+        if (instr != 1){
+            printf("%c", str[i]); 
+        }
+    }
+    }else{ //if the second string is bigger than the first
+        for (i = 0; i < l; i++){ //iterate through second string length
+        int instr = 0;
+        for (j = 0; j < k; j++){ // iterate through first string length
+            if (str[j] == str2[i]){
+                instr = 1;
+                break;
+            }
+        } 
+        if (instr != 1){
+            printf("%c", str2[i]); 
+        }
+    }
+    }
+
+}
+
+int main(){
+    
+    char str[12] = {'H', 'i', ' ', 'E', 'S', 'E', ' ', '1', '2', '4', '!', '\0'};
+    char str2[5] = {'H', 'i', ' ', '!', '\0'};
+
+    same_char_remove(str, str2);
+
+}
